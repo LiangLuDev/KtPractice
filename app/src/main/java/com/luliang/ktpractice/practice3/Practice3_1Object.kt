3ff1996ab67dd6491ec73493238223e9e2407d36
LiangLuDev <LiangLuDev@gmail.com>
2018-05-02 17:37:45 +0800

    kotlin练习-对象-1

---
+++ app/src/main/java/com/luliang/ktpractice/practice3/Practice3_1Object.kt
@@ -0,0 +1,11 @@
+package com.luliang.ktpractice.practice3
+
+/**
+ * Created by LuLiang on 2018/5/2.
+ * @author LuLiang
+ * @github https://github.com/LiangLuDev
+ * 对象
+ */
+class Practice3_1Object{
+
+}
